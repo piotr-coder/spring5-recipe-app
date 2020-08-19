0a4b4845da274ebe7787c0e385f2eab0c3789671
piotr-coder <blichu@wp.pl>
2020-08-19 18:55:55 +0200

    adding SpringBootServletInitializer

--- src/main/java/spring/Spring5RecipeAppApplication.java
+++ src/main/java/spring/Spring5RecipeAppApplication.java
@@ -2,9 +2,10 @@ package spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Spring5RecipeAppApplication {
+public class Spring5RecipeAppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Spring5RecipeAppApplication.class, args);
